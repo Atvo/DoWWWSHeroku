60eaa5f933ecbb55ca03f658a7c9bb17c832b19a
Atvo <atro.himanen@aalto.fi>
2015-12-27 21:07:20 +0200

    Heroku static test

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -130,7 +130,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap3"
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')#'static'
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, '/static/')#'/static/'
 
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
