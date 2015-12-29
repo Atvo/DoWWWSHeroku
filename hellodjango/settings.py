6c16d2555f1ebc9587864bfd5933c8ae624f8c38
Atvo <atro.himanen@aalto.fi>
2015-12-29 20:13:26 +0200

    Test changes in Heroku

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(__file__) # this is not Django setting.
 
 #ON HEROKU
-#DATABASE_URL = 'postgresql:///postgresql'
+DATABASE_URL = 'postgresql:///postgresql'
 
 
 # TEMPLATE_DIRS = (
@@ -94,12 +94,12 @@ WSGI_APPLICATION = 'hellodjango.wsgi.application'
 #}
 
 # ON LOCAL
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'mydatabase',
+#     }
+# }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -122,7 +122,7 @@ USE_TZ = True
 import dj_database_url
 
 #ON HEROKU
-#DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
+DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
