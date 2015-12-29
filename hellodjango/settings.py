b88625e00914162a4f56c6106a838abb19432dfc
Atvo <atro.himanen@aalto.fi>
2015-12-29 20:41:24 +0200

    Heroku

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -124,7 +124,8 @@ import dj_database_url
 #ON HEROKU
 import os
 if os.getcwd() == "/app":
-    DATABASES['default'] =  dj_database_url.config()
+    DATABASES = {'default': dj_database_url.config()}
+    #DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
