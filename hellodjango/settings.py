b61d95cf985e8c4803d61e6229cf19adf5ce5278
Atvo <atro.himanen@aalto.fi>
2015-12-29 20:27:14 +0200

    Heroku

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -87,11 +87,11 @@ WSGI_APPLICATION = 'hellodjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-#DATABASES = {
- #   'default': {
- #       'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#    }
-#}
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+   }
+}
 
 # ON LOCAL
 # DATABASES = {
